3a24e50874f902e273d7efa6fcd0f131013abd9c
PC <PC@DESKTOP-B4NGU8F>
2025-02-25 11:45:03 -0600

    se ha agregado varias cosas

--- AnalizadorSintactico.py
+++ AnalizadorSintactico.py
@@ -1,13 +1,16 @@
+preanalisis = ''  # Declaración global
+
 def Lista():
     digito()
     Resto_Lista()
 
 def Resto_Lista():
-    if preanilisis == '+':
+    global preanalisis  # Declaración global
+    if preanalisis == '+':
         coincidir('+')
         digito()
         Resto_Lista()
-    elif preanilisis == '-':
+    elif preanalisis == '-':
         coincidir('-')
         digito()
         Resto_Lista()
@@ -15,25 +18,49 @@ def Resto_Lista():
         pass
 
 def digito():
-    if preanilisis == '0':
+    global preanalisis  # Declaración global
+    if preanalisis == '0':
         coincidir('0')
-    elif preanilisis == '1':
+    elif preanalisis == '1':
         coincidir('1')
-    elif preanilisis == '2':
+    elif preanalisis == '2':
         coincidir('2')
-    elif preanilisis == '3':
+    elif preanalisis == '3':
         coincidir('3')
-    elif preanilisis == '4':
+    elif preanalisis == '4':
         coincidir('4')
-    elif preanilisis == '5':
+    elif preanalisis == '5':
         coincidir('5')
-    elif preanilisis == '6':
+    elif preanalisis == '6':
         coincidir('6')
-    elif preanilisis == '7':
+    elif preanalisis == '7':
         coincidir('7')
-    elif preanilisis == '8':
+    elif preanalisis == '8':
         coincidir('8')
-    elif preanilisis == '9':
+    elif preanalisis == '9':
         coincidir('9')
     else:
         print("no coincide la cadena")
+
+def coincidir(caracter):
+    global preanalisis  # Declaración global
+    if caracter == preanalisis:
+        return True
+    else:
+        return False
+
+def verificar_cadena(cadena):
+    global preanalisis  # Declaración global
+    for i in range(len(cadena)):
+        preanalisis = cadena[i] 
+        Lista()  
+        if not coincidir(cadena[i]):
+            print ("la cadena no cumple con los procesos")  # Se actualiza solo si la función retorna True
+            break  # O podrías usar 'continue' si quieres que solo lo ignore y siga
+
+def main():
+    cadena = input("Ingresa una cadena: ")
+    verificar_cadena(cadena)
+
+preanalisis = ''  # Inicia la variable global
+main()
